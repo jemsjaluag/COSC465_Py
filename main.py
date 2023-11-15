d0baf3f1e59fcbf5284379e059537ead341bb7b1
James Jaluag <james_jaluag@yahoo.com>
2023-11-15 10:40:22 -0500

    added stylesheet (no styles yet)

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sys
+from pathlib import Path
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 
 # window dimensions
@@ -32,6 +34,10 @@ class Form2(QDialog):
     def __init__(self, parent=None):
         super(Form2, self).__init__(parent)
 
+        ##### stylesheets
+        self.setStyleSheet(Path('signup.qss').read_text())
+        
+
         textBoxWidth = 80
         textBoxHeight = 30
 
@@ -41,71 +47,60 @@ class Form2(QDialog):
         ##### add widgets
         # username
         self.usernameLabel = QLabel("Username")
+        font = QFont("Calibri", 13)
+        self.usernameLabel.setFont(font)
         self.usernameLabel.setFrameStyle(QFrame.Sunken)
         self.usernameLabel.setAlignment(Qt.AlignBottom | Qt.AlignLeft)
 
         self.userName = QLineEdit()
         self.userName.setMinimumSize(textBoxWidth,textBoxHeight)
-        self.userName.move(20,20)
-        self.usernameLabel.setBuddy(self.userName)
 
-        # password
+        ### password
         self.passwordLabel = QLabel("Password")
         self.password = QLineEdit()
         self.password.setMinimumSize(textBoxWidth,textBoxHeight)
 
-        # first name
+        ### first name
         self.nameBox = QLineEdit("First Name")
         self.nameBox.setMinimumSize(textBoxWidth,textBoxHeight)
 
-        # last name
+        ### last name
         self.lastNameBox = QLineEdit("Last Name")
         self.lastNameBox.setMinimumSize(textBoxWidth,textBoxHeight)
 
-        # preferred sport
+        ### preferred sport
         self.preferredSport = QLineEdit("Preferred Sport")
         self.preferredSport.setMinimumSize(textBoxWidth,textBoxHeight)
 
-        # age
+        ### age
         self.age = QLineEdit("Age") 
         self.age.setMinimumSize(textBoxWidth,textBoxHeight)
 
         # creating the form layout
-        formLayout = QFormLayout()
-        formLayout.addRow(self.usernameLabel, self.userName)
-        formLayout.addRow(self.passwordLabel, self.password)
-
-        self.formGroupBox.setLayout(formLayout)
-
-        # button
+        self.createFormFormat()
+        
+        ### button
         self.button = QPushButton("Submit")
         buttonX = self.button.x()
         buttonY = self.button.y()
         self.button.setMaximumSize(80, 40)
         self.button.move(maxWidth / 2, maxHeight)
 
-        # create form
-        #self.createForm()
+        # main layout
+        mainLayout = QVBoxLayout()
+        mainLayout.addWidget(self.formGroupBox)
 
-        # layout
-        layout = QVBoxLayout()
-        layout.addWidget(self.formGroupBox)
-
-        """
-        layout.addWidget(self.usernameLabel)
-        layout.addWidget(self.nameBox)
-        layout.addWidget(self.lastNameBox)
-        layout.addWidget(self.userName)
-        layout.addWidget(self.password)
-        layout.addWidget(self.preferredSport)
-        layout.addWidget(self.age)
-        layout.addWidget(self.button)
-        """
-
-        self.setLayout(layout)
+        self.setLayout(mainLayout)
 
 
+    ### creates the form format
+    def createFormFormat(self):
+        formLayout = QFormLayout()
+        formLayout.addRow(self.usernameLabel, self.userName)
+        formLayout.addRow(self.passwordLabel, self.password)
 
+
+        self.formGroupBox.setLayout(formLayout)
 
 
 
