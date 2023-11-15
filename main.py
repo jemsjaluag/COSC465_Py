d3f3946c3fbd85f663e34201ba0dcaec3a386944
James <james_jaluag@yahoo.com>
2023-11-14 21:29:00 -0500

    from laptop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 from PyQt5.QtWidgets import (QLineEdit, QPushButton, QApplication,
-    QVBoxLayout, QDialog)
+    QVBoxLayout, QDialog, QMainWindow)
 
 class Form(QDialog):
 
@@ -22,11 +22,42 @@ class Form(QDialog):
     def greetings(self):
         print(f"Hello Hello Hello{self.edit.text()}")
 
+
+class Form2(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form2, self).__init__(parent)
+
+        # add widgets
+        self.nameBox = QLineEdit("First Name")
+        self.nameBox.setMinimumSize(100,50)
+
+
+        self.lastNameBox = QLineEdit("Last Name")
+
+        # layout
+        layout = QVBoxLayout()
+        layout.addWidget(self.nameBox, stretch=1)
+        layout.addWidget(self.lastNameBox, stretch=3)
+        
+
+        self.setLayout(layout)
+
+
+
+
+
+
 if __name__ == '__main__':
     # Create the Qt Application
     app = QApplication(sys.argv)
     # Create and show the form
-    form = Form()
-    form.show()
+    form = Form2()
+    #form.show()
+
+    window = QMainWindow()
+    window.addWidget(form)
+    window.resize(500, 500)
+    window.show()
     # Run the main Qt loop
     sys.exit(app.exec())
